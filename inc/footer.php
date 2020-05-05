f8841fe6ff873d91ed383e4af80b6fb59780d6c3
Sergio Alegre <sergio.alegre.arribas@gmail.com>
2020-05-05 09:39:18 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
 <footer class="pie">
-	<div>Sergio Alegre - <a href="https://twitter.com/naarean">@naarean</a> - <a href="https://github.com/naarean">github.com/naarean</a></div>
+	<div>Sergio Alegre - <a href="https://twitter.com/naarean">@naarean</a> - <a href="https://github.com/sergioalegre">github.com/sergioalegre</a></div>
 </footer>
